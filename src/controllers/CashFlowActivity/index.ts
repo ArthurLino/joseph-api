685c0ec6edab6ba1fb7934dd091f3a20ddbc3f98
ArthurLino <arthur.sslino@gmail.com>
2024-12-16 19:25:23 -0300

    refactoring imports

---
+++ src/controllers/CashFlowActivity/index.ts
@@ -0,0 +1,6 @@
+import { CreateActivityController } from "@activityControllers/CreateActivityController";
+import { ListActivitiesController } from "@activityControllers/ListActivitiesController";
+import { DeleteActivityController } from "@activityControllers/DeleteActivityController";
+import { UpdateActivityController } from "@activityControllers/UpdateActivityController";
+
+export { CreateActivityController, ListActivitiesController, DeleteActivityController, UpdateActivityController };
